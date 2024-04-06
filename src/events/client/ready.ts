ee91239cf52ee82f556f85fa8f38c611c442eb66
DarkslayerHaos <ccmanhacker@gmail.com>
2024-04-06 17:31:34 -0300

    just packages update

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -3,10 +3,10 @@ import { BaseClient } from '../../structures/BaseClient';
 import { BaseEvent } from '../../structures/BaseEvent';
 
 export default class Ready extends BaseEvent {
-    public execute(client: BaseClient) {
+    public async execute(client: BaseClient) {
         client.user?.setActivity(`${process.env.PREFIX as string}play`, { type: ActivityType.Listening });
         //Starting the Ruvyrias client
-        client.manager.init(client);
+        await client.manager.init(client);
 
         console.clear();
         console.log(`[+] Connected as ${client.user?.tag}`);
